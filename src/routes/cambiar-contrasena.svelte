783c66289868d69c4fc057aaf4bd90be2749a40a
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2021-11-22 16:39:06 -0400

    Update cambiar-contraseña

--- src/routes/cambiar-contrasena.svelte
+++ src/routes/cambiar-contrasena.svelte
@@ -1,9 +1,65 @@
-<div class="container">bg</div>
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import FormWrapper from '$lib/components/FormWrapper.svelte';
+	import '$lib/styles/login-signup.postcss';
+</script>
+
+<div class="container">
+	<div class= "contrasena-box">
+		<div class="contrasena-box-activa">
+			<FormWrapper title="Cambiar Contraseña">
+				
+				<span class=" "
+					>o <a href="/iniciar-sesion" class="text-coral">Iniciar sesion</a>
+				</span>
+
+				<Input
+					icon={'/assets/icons/key.svg'}
+					type="password"
+					classes=""
+					placeholder="Nueva Contraseña"
+					wrapperClasses="mt-3"
+				/>
+	
+				<Input
+					icon={'/assets/icons/key.svg'}
+					type="password"
+					classes=""
+					placeholder="Validar nueva contraseña"
+					wrapperClasses="mt-3"
+				/>
+
+				<Input
+					icon={'/assets/icons/key.svg'}
+					type="password"
+					classes=""
+					placeholder="Contasena anterior"
+					wrapperClasses="mt-3"
+				/>
+
+				<Button name="Enviar" type="submit" classes="mt-4" />
+			</FormWrapper>
+		</div>
+	</div>
+</div>
 
 <style lang="postcss">
+
 	.container {
-		height: 100%;
-		width: 100%;
 		background: url('/assets/bg-2.png');
 	}
+
+	.contrasena-box {
+		width: 900px;
+	}
+
+	.contrasena-box-activa{
+		height: 800px;
+    	position: relative;
+    	box-sizing: border-box;
+    	padding: 130px 130px;
+		transform: translate(20%, -10%);
+	}
+
 </style>
